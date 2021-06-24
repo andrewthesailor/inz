e245c1528ec063cfff477c11df06e706bc7afc5d
Andrew Szczepanik <andrew.szczepanik@outlook.com>
2021-06-24 10:13:53 +0200

    temp

--- src/main/java/com/szczepanika/scheduler/controller/UserController.java
+++ src/main/java/com/szczepanika/scheduler/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.szczepanika.scheduler.controller;
 
+import com.szczepanika.scheduler.model.parameters.Teacher;
 import com.szczepanika.scheduler.service.TimetableService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +18,8 @@ public class UserController {
     TimetableService timetableService;
 
     @RequestMapping("/user")
-    public String userPage(Model model){
-
+    public String userPage(Model model, @AuthenticationPrincipal Teacher teacher){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         model.addAttribute();
         return "userPage";
     }
---
+++ src/main/java/com/szczepanika/scheduler/service/impl/TimetableServiceImpl.java
@@ -0,0 +1,12 @@
+package com.szczepanika.scheduler.service.impl;
+
+import com.szczepanika.scheduler.service.TimetableService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TimetableServiceImpl implements TimetableService {
+
+
+
+
+}
